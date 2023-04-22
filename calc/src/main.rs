da577486c0f127a6e9a7de4bfc3fb267005d62fe
forfd8960 <forfd8960@gmail.com>
2023-04-22 23:31:30 +0800

    Impl Scanner for calc

--- calc/src/main.rs
+++ calc/src/main.rs
@@ -8,13 +8,16 @@ enum TokenType {
     Div(String),
     Mod(String),
     Pow(String),
-    Num(String, i64),
+    Num(String, f64),
+    Dot(String),
+    Eof,
 }
 
 #[derive(Debug)]
 enum Error {
     ParentNotMatch,
     ParseNumFailed(String),
+    TokenNotSupported(String),
 }
 
 #[derive(Debug)]
@@ -22,11 +25,12 @@ struct Scanner {
     chars: Vec<char>,
     start: i32,
     current: i32,
+    tokens: Vec<TokenType>,
 }
 
 struct Execute {
     tokens: Vec<TokenType>,
-    result_stack: Vec<i64>,
+    result_stack: Vec<f64>,
     op_stack: Vec<TokenType>,
     current: i32,
 }
@@ -36,7 +40,7 @@ impl Execute {
         Self {
             tokens: tokens,
             current: 0,
-            result_stack: Vec::<i64>::new(),
+            result_stack: Vec::<f64>::new(),
             op_stack: Vec::<TokenType>::new(),
         }
     }
@@ -52,12 +56,112 @@ impl Scanner {
             chars: chars,
             start: 0,
             current: 0,
+            tokens: Vec::<TokenType>::new(),
         };
     }
 
-    fn scan_tokens(&mut self) -> Vec<TokenType> {
-        let mut tokens = Vec::<TokenType>::new();
-        return tokens;
+    fn scan_tokens(&mut self) -> Result<(), Error> {
+        while !self.is_end() {
+            self.start = self.current;
+            if let Err(e) = self.scan_token() {
+                return Err(e);
+            }
+        }
+
+        self.add_token(TokenType::Eof);
+        return Ok(());
+    }
+
+    fn is_end(&self) -> bool {
+        self.current as usize >= self.chars.len()
+    }
+
+    fn scan_token(&mut self) -> Result<(), Error> {
+        let ch = self.advance();
+        let mut rs = Err(Error::TokenNotSupported(format!("{} is not supported", ch)));
+        match ch {
+            '(' => self.add_token(TokenType::LeftParent("(".to_string())),
+            ')' => self.add_token(TokenType::RightParent(")".to_string())),
+            '*' => self.add_token(TokenType::Multiply("*".to_string())),
+            '/' => self.add_token(TokenType::Div("/".to_string())),
+            '+' => self.add_token(TokenType::Plus("+".to_string())),
+            '-' => self.add_token(TokenType::Minus("-".to_string())),
+            '^' => self.add_token(TokenType::Pow("^".to_string())),
+            '.' => self.add_token(TokenType::Dot(".".to_string())),
+            _ => {
+                if ch.is_digit(10) {
+                    match self.get_number_token() {
+                        Ok(tk) => {
+                            self.add_token(tk);
+                            rs = Ok(())
+                        }
+                        Err(e) => rs = Err(e),
+                    }
+                }
+            }
+        }
+        rs
+    }
+
+    fn add_token(&mut self, tk_type: TokenType) {
+        self.tokens.push(tk_type)
+    }
+
+    fn advance(&mut self) -> char {
+        let current_ch = self.chars.get(self.current as usize).unwrap();
+        self.current += 1;
+        *current_ch
+    }
+
+    fn is_digit(&self, ch: char) -> bool {
+        ch.is_digit(10)
+    }
+
+    fn get_number_token(&mut self) -> Result<TokenType, Error> {
+        while let Some(ch) = self.peek() {
+            if !self.is_digit(ch) {
+                break;
+            }
+            self.advance();
+        }
+
+        if let Some(ch) = self.peek() {
+            if ch == '.' {
+                let next_tk = self.peek_next();
+                if next_tk.is_some() && self.is_digit(next_tk.unwrap()) {
+                    self.advance();
+
+                    while let Some(ch) = self.peek() {
+                        if !self.is_digit(ch) {
+                            break;
+                        }
+                        self.advance();
+                    }
+                }
+            }
+        }
+
+        let ches = &self.chars[(self.start as usize)..(self.current as usize)];
+        let num_str = String::from_iter(ches);
+        match num_str.parse::<f64>() {
+            Ok(num) => Ok(TokenType::Num(num_str, num)),
+            Err(err) => Err(Error::ParseNumFailed(err.to_string())),
+        }
+    }
+
+    fn peek(&self) -> Option<char> {
+        if self.is_end() {
+            return None;
+        }
+
+        Some(self.chars[self.current as usize])
+    }
+
+    fn peek_next(&self) -> Option<char> {
+        if (self.current + 1) as usize >= self.chars.len() {
+            return None;
+        }
+        Some(self.chars[(self.current + 1) as usize])
     }
 
     fn parse_num(&mut self) {}
